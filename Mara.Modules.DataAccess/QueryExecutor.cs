a73ff61366a7a334359c3f0134cd38536dcf8521
Mara-AspIT <mara@aspit.dk>
2017-11-29 13:14:51 +0100

    Changed target to .NET 4.7.1 and C# ver. to 7.1.

--- Mara.Modules.DataAccess/QueryExecutor.cs
+++ Mara.Modules.DataAccess/QueryExecutor.cs
@@ -1,8 +1,8 @@
 ﻿/**************************************************************************************************
 *  Author: Mads Mikkel Rasmussen (mara@aspit.dk), github: https://github.com/Mara-AspIT/          *
-*  Solution: .NET version: 4.7, C# version: 7.0                                                   *
-*  Visual Studio version: Visual Studio Enterprise 2017, version 15.4.0                           *
-*  Repository: Not yet available                                                                  *
+*  Solution: .NET version: 4.7.1, C# version: 7.1                                                 *
+*  Visual Studio version: Visual Studio Enterprise 2017, version 15.4.5                           *
+*  Repository: https://github.com/Mara-AspIT/Mara.Modules.git                                     *
 **************************************************************************************************/
 using System;
 using System.Data;
